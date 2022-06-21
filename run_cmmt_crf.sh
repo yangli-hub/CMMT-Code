6d399657c0e06d48d99865f255dd1ab2a06760a4
yangli-hub <70850281+yangli-hub@users.noreply.github.com>
2022-06-21 12:04:04 +0800

    Update run_cmmt_crf.sh

--- run_cmmt_crf.sh
+++ run_cmmt_crf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-for i in 'twitter2015' 'twitter2017' #  'twitter2015'
+for i in 'twitter2015' 'twitter2017' 
 do
-  echo 'run_mtmner_crf.py'
+  echo 'run_cmmt_crf.py'
   echo ${i}
   echo ${k}
   PYTHONIOENCODING=utf-8 CUDA_VISIBLE_DEVICES=0 python run_cmmt_crf.py --task_name=${i} 
